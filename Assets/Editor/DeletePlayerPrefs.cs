c9940e1748fff98b1c456afd83233836919fc83e
aprilzhang <aprilsulu@gmail.com>
2016-11-01 21:00:15 +0000

    Add main menu scenes etc.

---
+++ Assets/Editor/DeletePlayerPrefs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class DeletePlayerPrefs : ScriptableObject {
+
+	[MenuItem ("Editor/Delete Player Prefs")]
+	static void DeletePrefs()
+	{
+		if (EditorUtility.DisplayDialog ("Delete all player preferences?",
+			   "Are you sure you want to delete all the player preferences, this action cannot be undone." +
+			   "Yes",
+			   "No")) {
+			PlayerPrefs.DeleteAll ();
+		}
+	}
+}
---
+++ Assets/Editor/GameManagerEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+
+		GameManager myGM = (GameManager)target;
+
+		if (GUILayout.Button ("Reset Player State")) {
+			PlayerPrefManager.ResetPlayerState (myGM.startLives, false);
+		}
+
+		if (GUILayout.Button ("Reset Highscore")) {
+			PlayerPrefManager.SetHighscore(0);
+		}
+
+		if (GUILayout.Button ("Output Player State")) {
+			PlayerPrefManager.ShowPlayerPrefs();
+		}
+	}
+}
